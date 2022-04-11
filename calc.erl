6f09dedb29723f03658e99d7bc6d7562fb75f76a
Markus Gabryel Mello da Silva <69471356+markus-gabryel@users.noreply.github.com>
2022-04-11 13:44:34 -0700

    Move each operation to different process

--- calc.erl
+++ calc.erl
@@ -1,18 +1,78 @@
 -module(calc).
--export([addition/2, subtraction/2, multiplication/2, division/2, clear/0]).
+-export([
+	 addition/0,
+	 subtraction/0,
+	 multiplication/0,
+	 division/0,
 
-addition(X, Y) ->
-	X + Y.
+	 cls/0,
+	 listen/4,
+	 start/0
+]).
 
-subtraction(X, Y) ->
-	X - Y.
+addition() ->
+	io:format("addition: started~n"),
+	receive
+		{From, X, Y} ->
+			io:format("addition: receive ~p and ~p from ~p~n", [X, Y, From]),
+			From ! {result, X + Y},
+			addition()
+	end.
 
-multiplication(X, Y) ->
-	X * Y.
+subtraction() ->
+	io:format("subtraction: started~n"),
+	receive
+		{From, X, Y} ->
+			io:format("subtraction: receive ~p and ~p from ~p~n", [X, Y, From]),
+			From ! {result, X - Y},
+			subtraction()
+	end.
 
-division(X, Y) ->
-	X / Y.
+multiplication() ->
+	io:format("multiplication: started~n"),
+	receive
+		{From, X, Y} ->
+			io:format("multiplication: receive ~p and ~p from ~p~n", [X, Y, From]),
+			From ! {result, X * Y},
+			multiplication()
+	end.
 
-clear() ->
-	io:format("\e[H\e[J").
+division() ->
+	io:format("division: started~n"),
+	receive
+		{From, X, Y} ->
+			io:format("division: receive ~p and ~p from ~p~n", [X, Y, From]),
+			From ! {result, X / Y},
+			division()
+	end.
+
+%% ----------------------------------------------------------------------------
+
+cls() -> io:format("\e[H\e[J").
+
+listen(AddPid, SubPid, MulPid, DivPid) ->
+	receive
+		{result, Value} ->
+			io:format("= ~p~n", [Value]);
+
+		{'+', X, Y} ->
+			AddPid ! {self(), X, Y},
+			listen(AddPid, SubPid, MulPid, DivPid);
+		{'-', X, Y} ->
+			SubPid ! {self(), X, Y},
+			listen(AddPid, SubPid, MulPid, DivPid);
+		{'*', X, Y} ->
+			MulPid ! {self(), X, Y},
+			listen(AddPid, SubPid, MulPid, DivPid);
+		{'/', X, Y} ->
+			DivPid ! {self(), X, Y},
+			listen(AddPid, SubPid, MulPid, DivPid)
+	end.
+
+start() ->
+	AddPid	= spawn(calc, addition, []),
+	SubPid	= spawn(calc, subtraction, []),
+	MulPid	= spawn(calc, multiplication, []),
+	DivPid	= spawn(calc, division, []),
+	spawn(calc, listen, [AddPid, SubPid, MulPid, DivPid]).
 
