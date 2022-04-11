2db263288c539f5a2691608dc8c693fad4bc74a8
Markus Gabryel Mello da Silva <69471356+markus-gabryel@users.noreply.github.com>
2022-04-11 15:57:09 -0700

    Add interpreter and cascade computing

--- calc.erl
+++ calc.erl
@@ -5,74 +5,118 @@
 	 multiplication/0,
 	 division/0,
 
-	 cls/0,
-	 listen/4,
-	 start/0
+	 interpret/1,
+	 compute/1,
+	 start/0,
+	 cls/0
 ]).
 
+%% ----------------------------------------------------------------------------
+
 addition() ->
-	io:format("addition: started~n"),
+	io:format("addition: (re)started~n"),
 	receive
 		{From, X, Y} ->
-			io:format("addition: receive ~p and ~p from ~p~n", [X, Y, From]),
-			From ! {result, X + Y},
+			Result = X + Y,
+			io:format("addition: from ~p: ~p + ~p = ~p~n",
+				  [From, X, Y, Result]),
+			From ! {result, Result},
 			addition()
 	end.
 
 subtraction() ->
-	io:format("subtraction: started~n"),
+	io:format("subtraction: (re)started~n"),
 	receive
 		{From, X, Y} ->
-			io:format("subtraction: receive ~p and ~p from ~p~n", [X, Y, From]),
-			From ! {result, X - Y},
+			Result = X - Y,
+			io:format("subtraction: from ~p: ~p - ~p = ~p~n",
+				  [From, X, Y, Result]),
+			From ! {result, Result},
 			subtraction()
 	end.
 
 multiplication() ->
-	io:format("multiplication: started~n"),
+	io:format("multiplication: (re)started~n"),
 	receive
 		{From, X, Y} ->
-			io:format("multiplication: receive ~p and ~p from ~p~n", [X, Y, From]),
-			From ! {result, X * Y},
+			Result = X * Y,
+			io:format("multiplication: from ~p: ~p * ~p = ~p~n",
+				  [From, X, Y, Result]),
+			From ! {result, Result},
 			multiplication()
 	end.
 
 division() ->
-	io:format("division: started~n"),
+	io:format("division: (re)started~n"),
 	receive
 		{From, X, Y} ->
-			io:format("division: receive ~p and ~p from ~p~n", [X, Y, From]),
-			From ! {result, X / Y},
+			Result = X / Y,
+			io:format("division: from ~p: ~p / ~p = ~p~n",
+				  [From, X, Y, Result]),
+			From ! {result, Result},
 			division()
 	end.
 
 %% ----------------------------------------------------------------------------
 
-cls() -> io:format("\e[H\e[J").
+interpret(Expression) -> interpret(Expression, 0).
+	
+interpret([], Buffer) -> Buffer;
 
-listen(AddPid, SubPid, MulPid, DivPid) ->
-	receive
-		{result, Value} ->
-			io:format("= ~p~n", [Value]);
+interpret([Current | Rest], Buffer) ->
+	if
+		[Current] == "0" ;
+		[Current] == "1" ;
+		[Current] == "2" ;
+		[Current] == "3" ;
+		[Current] == "4" ;
+		[Current] == "5" ;
+		[Current] == "6" ;
+		[Current] == "7" ;
+		[Current] == "8" ;
+		[Current] == "9" ->
+			Value = element(1, string:to_integer([Current])),
+			interpret(Rest, Buffer * 10 + Value);
 
-		{'+', X, Y} ->
-			AddPid ! {self(), X, Y},
-			listen(AddPid, SubPid, MulPid, DivPid);
-		{'-', X, Y} ->
-			SubPid ! {self(), X, Y},
-			listen(AddPid, SubPid, MulPid, DivPid);
-		{'*', X, Y} ->
-			MulPid ! {self(), X, Y},
-			listen(AddPid, SubPid, MulPid, DivPid);
-		{'/', X, Y} ->
-			DivPid ! {self(), X, Y},
-			listen(AddPid, SubPid, MulPid, DivPid)
+		[Current] == "+" ;
+		[Current] == "-" ;
+		[Current] == "*" ;
+		[Current] == "/" ->
+			Operator = list_to_atom([Current]),
+			SubExpression = interpret(Rest, 0),
+			{Operator, Buffer, SubExpression}
 	end.
 
-start() ->
-	AddPid	= spawn(calc, addition, []),
-	SubPid	= spawn(calc, subtraction, []),
-	MulPid	= spawn(calc, multiplication, []),
-	DivPid	= spawn(calc, division, []),
-	spawn(calc, listen, [AddPid, SubPid, MulPid, DivPid]).
+%% ----------------------------------------------------------------------------
 
+compute(Expression) when is_integer(Expression) ; is_float(Expression) ->
+	Expression;
+
+compute(Expression) when is_tuple(Expression) ->
+	case Expression of
+		{'+', X, Y} ->
+			addition ! {self(), compute(X), compute(Y)};
+		{'-', X, Y} ->
+			subtraction ! {self(), compute(X), compute(Y)};
+		{'*', X, Y} ->
+			multiplication ! {self(), compute(X), compute(Y)};
+		{'/', X, Y} ->
+			division ! {self(), compute(X), compute(Y)}
+	end,
+
+	receive
+		{result, Value} ->
+			Value
+	end;
+
+compute(Expression) -> compute(interpret(Expression)).
+
+%% ----------------------------------------------------------------------------
+
+start() ->
+	register(addition, spawn(calc, addition, [])),
+	register(subtraction, spawn(calc, subtraction, [])),
+	register(multiplication, spawn(calc, multiplication, [])),
+	register(division, spawn(calc, division, [])).
+
+cls() -> io:format("\e[H\e[J").
